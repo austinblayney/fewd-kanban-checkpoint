581fae8eafbb63eb72a815c79fca66f79f30971f
austin <austin.blayney22@gmail.com>
2019-04-03 19:53:49 -0600

    build 2

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,58 +1,63 @@
 <template>
-<div class="col-6 col-md-3">
-   <div class="card">
-     <div class="card-header d-flex">
-       <h3>{{listData.title}}</h3>
-       <button @click="deleteList">Delete</button>
-     </div>
-     <div class="card-body">
-        <task v-for="task in tasks" :task="task"/>
+  <div class=" list col-6 col-md-3">
+    <div class="card">
+      <div class="card-header d-flex">
+        <h3>{{listData.title}}</h3>
+        <button @click="deleteList">Delete</button>
+      </div>
+      <div class="card-body">
+        <task v-for="task in tasks" :task="task" />
         <form @submit.prevent="addTask">
           <input type="text" required v-model="newTask.title" placeholder="Add a task">
           <button type="submit">Submit</button>
         </form>
-     </div>
-   </div>
-</div>  
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Task from '@/components/Task.vue'
-export default {
-  name: 'List',
-  props: ['listData'],
-  data() {
-    return {
-      newTask: {
-        title: ''
+  import Task from '@/components/Task.vue'
+  export default {
+    name: 'List',
+    props: ['listData'],
+    data() {
+      return {
+        newTask: {
+          title: ''
+        }
       }
-    }
-  },
-  computed: {
-    tasks() {
-      return this.$store.state.tasks[this.listData._id] || []
-    }
-  },
-  methods: {
-    deleteList() {
-      this.$store.dispatch('deleteList', this.listData)
     },
-    addTask() {
-      //object destructuring, this is the same as with actions in the store
-      let { boardId, _id: listId } = this.listData
-      this.$store.dispatch('addTask', {
-        ...this.newTask,
-        boardId,
-        listId
-      })
-      this.newTask.title = ''
+    computed: {
+      tasks() {
+        return this.$store.state.tasks[this.listData._id] || []
+      }
+    },
+    methods: {
+      deleteList() {
+        this.$store.dispatch('deleteList', this.listData)
+      },
+      addTask() {
+        //object destructuring, this is the same as with actions in the store
+        let { boardId, _id: listId } = this.listData
+        this.$store.dispatch('addTask', {
+          ...this.newTask,
+          boardId,
+          listId
+        })
+        this.newTask.title = ''
+      }
+    },
+    components: {
+      Task
     }
-  },
-  components: {
-    Task
   }
-}
 </script>
 
 <style scoped>
+  .list {
+    background-image: url("https://photos.google.com/photo/AF1QipOwnBWSEw8Tn8cC5yKASWdIUMlOmfL_8tQc54ks");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 </style>
--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -3,10 +3,10 @@
     {{board.title}}
     <div class="row">
       <list v-for="list in lists" :listData='list'></list>
-    <form class="col-6 col-md-3 card" @submit.prevent="addList">
-      <input v-model="newList.title" type="text" required placeholder="List Title">
-      <button type="submit">Submit</button>
-    </form>
+      <form class="col-6 col-md-3 card" @submit.prevent="addList">
+        <input v-model="newList.title" type="text" required placeholder="List Title">
+        <button type="submit">Submit</button>
+      </form>
     </div>
   </div>
 </template>
@@ -51,8 +51,10 @@
 </script>
 <style>
   .board {
-    background-image: url("https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/1922177_587923481301426_1171404669_n.jpg?_nc_cat=102&_nc_ht=scontent-sea1-1.xx&oh=6c9193cdd728760efdd546b9218ed30f&oe=5D02439F");
+    background-image: url("https://lh3.googleusercontent.com/_F7EsZp7K7FNaJQU_BZhzoFYbrlB7jRVChPgwkU1n096C6bhIE0EAwBrrD4n8rBotxph_qyUwLOHCunUSrZYen7ZNgyPUHO8j94NLjeNvdrmqEL3sPJxLfhl7scGiUvSyjgFkYPXZNRmG4ANSIWhr4VrY7aOtkAwLmoQ5r4oKPqBQ8jmfnCebqmKxSI6Eb-Psqiu4_FpV3cGl8UFHoV1wIXRAmvCMtaaTQqDurCTD-jGtElLIP8rmLEpIrqwEi7wmO1NgzbeplXMUhK5E78UC_VbRlRfNMTQlmySRSGCUtivLW36cBC96_WIrLQI9TBWWAE2w1cn9xEDKMCDnz9lyHtDd8DxPxI09apaKE5p458Pef4ScQOAyrlYFkGHGx1Z8n5fCd8SdzshOIGO4epIkOmQzKhkejWZXWsKGtdnOBCuM4qJTY8N4Mq6_Vi6d6uDwEbnNC56AnEaRTk4SviJmaXMuS-97kTMTsAr1qKAjvVyouDk2PySs-am0a5l7j4U77uU8SPN5QfVDnV8fIC6Lp1-WtcMdxqrhioRix41SRSO52qM6cG3S_m9vxpOZZ3z8BB29xGfViGrLtH620w3i05H3K7Fe-xHGSBUmNv6d64mh5Y5epZG8gE31QBxFAYqgODcawXowZAMVSOfZIAAeZcuOEvgWw=w497-h579-no");
     background-size: cover;
-    background-repeat: no-repeat
+    background-repeat: no-repeat;
+    min-height: 80vh;
+    background-position: center;
   }
 </style>
